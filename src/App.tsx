211c049fdf8d5839c934c605e60e3d433f1ce3e5
AbouYeo <kagnekoyeo@gmail.com>
2025-08-25 19:35:17 +0200

    Generer les cartes dynamiquement - prototype

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,31 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 function App() {
+    const allCharacters = [
+        {
+            image: "heros.jpg",
+            name: "Kikisan",
+            health: 40,
+            magic: 35,
+            power: 60,
+        },
+        {
+            image: "magicienne.jpg",
+            name: "Karaba",
+            health: 50,
+            magic: 75,
+            power: 40,
+        },
+    ];
+
     return (
         <main className="flex flex-col justify-between min-h-svh">
             <Navbar />
-            <Card />
+            <section className="flex justify-center items-center gap-8 flex-wrap">
+                {allCharacters.map((oneCharacter, index) => (
+                    <Card key={index} character={oneCharacter} />
+                ))}
+            </section>
             <Footer />
         </main>
     );
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,19 +1,48 @@
 import Statistiques from "./Statistiques";
 
-export default function Card() {
+type CardProps = {
+    character: Character;
+};
+
+type Character = {
+    image: string;
+    name: string;
+    health: number;
+    magic: number;
+    power: number;
+};
+
+export default function Card({ character }: CardProps) {
+    const allStats = [
+        { stat: "Santé", value: character.health, unity: "PV" },
+        { stat: "Magie", value: character.magic, unity: "PM" },
+        { stat: "Puissance", value: character.power, unity: "Atk" },
+    ];
+
+    console.table(character);
     return (
         <div className="ml-4 flex flex-col w-[250px] h-[400px] border border-neutral-500 overflow-hidden customShadow rounded-xl">
             <div className="w-[250px] h-[250px] overflow-hidden">
-                <img src="/src/assets/images/personnages/heros.jpg" alt="" />
+                <img
+                    src={`/src/assets/images/personnages/${character.image}`}
+                    alt=""
+                />
             </div>
             <div className="p-2">
                 <div>
-                    <p className="font-bold text-xl text-center">Kikisan</p>
-                    <Statistiques stat="Santé" value={30} unit="PV" />
-                    <Statistiques stat="Magie" value={30} unit="PM" />
-                    <Statistiques stat="Puissance" value={30} unit="Atk" />
+                    <p className="font-bold text-xl text-center">
+                        {character.name}
+                    </p>
+
+                    {allStats.map((oneStat) => (
+                        <Statistiques
+                            stat={oneStat.stat}
+                            value={oneStat.value}
+                            unit={oneStat.unity}
+                        />
+                    ))}
                 </div>
-                <div className="flex justify-between">
+                <div className="flex justify-between mt-2">
                     <button className="bg-blue-500 rounded-xl border-2 border-neutral-400 hover:border-neutral-700 hover:opacity-90 duration-300 px-2 py-1">
                         Attaquer
                     </button>
